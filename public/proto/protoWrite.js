54e395f469cbbbc24c71cb619542a8c1a9332e9e
Dylan <d_h@mit.edu>
2021-03-02 17:02:54 -0600

    protobuf test

---
+++ public/proto/protoWrite.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const Schema = require("./dotDensity_pb");
+
+const dots = new Schema.Dots();
+
+for (let i = 0; i < 1_000_000; i++){
+    const currentDot = new Schema.Dot()
+    currentDot.setRacecode(i%8)
+    currentDot.setLatitude(Math.round(Math.random()*1e10)/1e4)
+    currentDot.setLongitude(Math.round(Math.random()*1e10)/1e4)
+    dots.addDots(currentDot)
+}
+const bytes =  dots.serializeBinary()
+fs.writeFileSync("dotsBinary", bytes)
